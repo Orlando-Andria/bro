75b35f28d3e154fb9fa26978b486cbeb74a0f423
orlando <orlandomahery98@gmail.com>
2025-02-14 11:08:22 +0300

    commit

--- src/component/Client.tsx
+++ src/component/Client.tsx
@@ -1,39 +1,55 @@
-import Elon from"../image/Elon.jpeg"
-import Fazza from"../image/fazza.jpeg"
-import Mark from"../image/Mark.jpeg"
+import { useState } from "react";
+import Elon from "../image/Elon.jpeg";
+import Fazza from "../image/fazza.jpeg";
+import Mark from "../image/Mark.jpeg";
 
 type Client = {
-    id: number;
-    name: string;
-    contact: string;
-    image:string;
-  };
-  
-  const Clients: React.FC = () => {
-    const clients: Client[] = [
-      { id: 1, name: "Elon Musk", contact: "Elon@Gmail.com", image:Elon},
-      { id: 2, name: "Fazza hamdan", contact: "Fazza@Gmail.com", image:Fazza},
-      { id: 2, name: "Mark zuckerberg", contact: "Marck@Gmail.com", image:Mark},
+  id: number;
+  name: string;
+  contact: string;
+  image: string;
+  amount: number;
+};
 
-    ];
-  
-    return (
-      <div>
-        <h1>Clients</h1>
-        <ul className="clientlist">
-          {clients.map((client) => (
-            <li key={client.id} className="Client">
-              <img src={client.image} alt="" className="person" />
-             <div className="container_name">
-              <p className="client-name">{client.name} <br/>
-              {client.contact}</p>
-              </div> 
-              <button className="createbtn">Amount</button>
-            </li>
-          ))}
-        </ul>
-      </div>
+const Clients: React.FC = () => {
+  const [clients, setClients] = useState<Client[]>([
+    { id: 1, name: "Elon Musk", contact: "Elon@Gmail.com", image: Elon, amount: 300000 },
+    { id: 2, name: "Fazza Hamdan", contact: "Fazza@Gmail.com", image: Fazza, amount: 290000 },
+    { id: 3, name: "Mark Zuckerberg", contact: "Marck@Gmail.com", image: Mark, amount: 100500 },
+  ]);
+
+  const Amount = (id: number) => {
+    setClients((Clients) =>
+      Clients.map((client) =>
+        client.id === id ? { ...client, amount: client.amount + 500 } : client
+      )
     );
   };
-  
-  export default Clients;
+
+  return (
+    <div>
+      <h1>Clients</h1>
+      <ul className="clientlist">
+        {clients.map((client) => (
+          <li key={client.id} className="Client">
+            <img src={client.image} alt={client.name} className="person" />
+            <div className="container_name">
+              <p className="client-name">
+                {client.name} <br />
+                {client.contact}
+              </p>
+            </div>
+            <div className="container-amount">
+            <p className="montant"> ${client.amount}</p>
+            </div>
+            <button className="createbtn" onClick={() => Amount(client.id)}>
+               Amount
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Clients;
